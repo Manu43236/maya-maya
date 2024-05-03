3eba52eb4213a6684d83f80d0557b325896149b9
parth <contact.parthpitroda@gmail.com>
2024-05-03 10:38:26 +0530

    1: Implement splash screen, 2: implement signup screen, 3: implement sign-in screen. 4: create const for image, color and strings.

---
+++ lib/base/const/color_const.dart
@@ -0,0 +1,89 @@
+
+import 'dart:ui';
+
+const Color primary = Color(0xFF0F263B);
+const Color grey = Color(0xFF4A4D52);
+const Color lightGrey = Color(0xFF727576);
+const Color silver = Color(0xFF8C949E);
+const Color darkBlue = Color(0xFF050D19);
+const Color navyBlue = Color(0xFF0F263B);
+const Color black = Color(0xFF1A1818);
+const Color teal = Color(0xFF004C50);
+const Color darkTeal = Color(0xFF004554);
+const Color brown = Color(0xFF410F12);
+const Color purple = Color(0xFF632269);
+const Color white = Color(0xFFFFFFFF);
+const Color offWhite = Color(0xFFF5F1EE);
+
+Color questionFill = const Color(0xFF631E22);
+
+const Color resumeExpandableColor = Color(0xFFDED7CF);
+const Color resumeBgColor = Color(0xFFF4F2F0);
+
+const Color bottomNavBarColor = Color(0xFF0F1F2D);
+const Color badgeColor = Color(0xFFFE0101);
+
+const buttonBorderColor = Color(0xFFF5F5F5);
+const yellowColor = Color(0xFFFFD800);
+
+const redGradient = [
+  Color(0xFF672024),
+  Color(0xFF591A1D),
+  Color(0xFF3C0C0F),
+  Color(0xFF290306),
+];
+
+const tealGradientForSS = [
+  Color(0xFF1E4B4F),
+  Color(0xFF163137),
+  Color(0xFF142A31),
+  Color(0xFF11232B),
+  Color(0xFF0F1E26),
+  Color(0xFF0C141D),
+
+];
+
+const tealGradient = [
+  Color(0xFF1E4B4F),
+  Color(0xFF0C141D),
+];
+
+
+const darkBlueGradient = [
+  Color(0xFF050D19),
+  // Color(0xFF0F273C),
+  Color(0xFF0E2438),
+];
+
+const darkBlueGradient2 = [
+  Color(0xFF0E2438),
+  Color(0xFF050D19),
+];
+
+const darkBlueGradient3 = [
+  Color(0xFF0F273C),
+  Color(0xFF0C1E30),
+  Color(0xFF081423),
+  Color(0xFF050D19),
+];
+
+const authenticationButtonsGradient = [
+  Color(0xFF0E2438),
+  Color(0xFF050D19),
+  Color(0xFF030507),
+];
+
+const bgGradient = [
+  Color(0xFF0E2337),
+  Color(0xFF09111D),
+];
+
+const authBgGradient = [
+  Color(0xFF0F273C),
+  Color(0xFF050D19),
+];
+
+const whiteGradient = [
+  Color(0xFFF5F1EE),
+  Color(0xFFE2DCD6),
+];
